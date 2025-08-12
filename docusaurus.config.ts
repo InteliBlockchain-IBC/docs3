b2cd1fad9e98b689e6b92f1a3208367ec6542542
Vinicius Testa Passos <viniciustestapassos@gmail.com>
2025-08-12 12:51:45 -0300

    feat: altera url base do dominio

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://github.com',
+  url: 'http://docs3.inteliblockchain.org/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/docs3/',
